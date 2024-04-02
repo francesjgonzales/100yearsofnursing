dd52f93bfe3f9b1a8ada54d27093421389371f11
francesjgonzales <francesj7837@gmail.com>
2024-04-01 21:42:28 -0600

    fixed video and theme pages

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,3 @@ items.forEach((el) => {
   }
 });
 
-// Testing JSON for article value
-// var str = "This is the first line\n\n\nand *this* is the second line";
-// console.log(str.replace(/_/g, "<i>"));
-
